20563b49ae59cd792502a2e9b52f172acd33177a
eder junio <ederjunio14@hotmailo.com>
2024-06-20 17:35:47 -0400

    Primeiro commit

---
+++ src/main/java/com/mycompany/testepraticoiniflex/TestePraticoIniflex.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testepraticoiniflex;
+
+/**
+ *
+ * @author ederjunio
+ */
+public class TestePraticoIniflex {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        
+    }
+}
---
+++ src/main/java/com/mycompany/testepraticoiniflex/modelo/funcionarioModelo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.testepraticoiniflex.modelo;
+
+/**
+ *
+ * @author ederjunio
+ */
+public class funcionarioModelo {
+    
+    public double salario;
+    public String funcao;
+    
+}
